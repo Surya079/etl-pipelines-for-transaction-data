1f30df1b0729bbbd30b11faa2c0eac6dc5e39627
Surya V <developer.surya03@gmail.com>
2026-08-29 20:51:04 +0530

    card details generated using faker module

---
+++ api/generators.py
@@ -0,0 +1,54 @@
+from faker import Faker
+import uuid
+import random
+import datetime
+from utills import get_random_date_utc
+from api.constants import (
+    CARD_NETWORKS,
+    CARD_TYPES,
+    CITIES,
+    COUNTRIES,
+    COUNTRIES_CITIES,
+    CURRENCIES,
+    MCC_CODES,
+    POS_ENTRY_MODES,
+    RESPONSE_CODES,
+    TRANSACTION_TYPES,
+    ACQUIRER_BANKS,
+    DEVICE_INFO,
+    ISSUER_BANKS,
+    MERCHANT_NAMES,
+)
+
+fake = Faker()
+
+
+def generate_transactions():
+    """
+        Generate a realistic transaction dictionary.
+
+    Returns:
+        dict: A dictionary with many transaction fields.
+
+    """
+    # Generate a base datetime (UTC) within the last 30 days
+    trans_dt_utc = get_random_date_utc()
+
+    offset_hours = random.random(-12, 14)
+    offset = datetime.timedelta(hours=offset_hours)
+    trans_dt_local = trans_dt_utc.astimezone(offset)
+
+    transaction_datetime = trans_dt_utc.isoformat()
+    loca_transaction_datetime = trans_dt_local.isoformat()
+
+    posting_date = trans_dt_utc.date().isoformat()
+
+    # card_details  
+    card_number_fulls = fake.credit_card_number().replace(" ", "")
+    card_number_masked = f"{card_number_fulls[:4]} **** **** {card_number_fulls[-4:]}"
+    card_expiry = fake.credit_card_expire(date_format="%m%y")
+    card_holder_name = fake.name()
+    card_type = random.choice(CARD_TYPES)
+    card_networks = random.choice(CARD_NETWORKS)
+
+    
